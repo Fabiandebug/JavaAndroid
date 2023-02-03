3d11fbe9e7bde741bc4b6bb0fa7e3c027289c9eb
Maina Evans <kinuthiafabiankinuthia@gmail.com>
2023-02-04 01:57:38 +0300

    Registration page

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onBtnclick(View view){
+//        Initialize the text viewer
+        TextView vtxtfirstnme =findViewById(R.id.vtxtfirstname);
+        TextView vtxtlastname = findViewById(R.id.vtxtlastname);
+        TextView vtxtemail = findViewById(R.id.vtxtemail);
+        TextView  vtxtusername = findViewById(R.id.vtxtusername);
+//Initialize the text editor
+        EditText txtusername =findViewById(R.id.edittxtname);
+        EditText txtfirstname = findViewById(R.id.edittxtFirstname);
+        EditText txtlastname = findViewById(R.id.edittxtlastname);
+        EditText txtemail = findViewById(R.id.edittxtemail);
+
+//        Pass data to the TextViewer
+
+        vtxtfirstnme.setText(txtfirstname.getText().toString());
+        vtxtlastname.setText(txtlastname.getText().toString());
+        vtxtemail.setText(txtemail.getText().toString());
+        vtxtusername.setText(txtusername.getText().toString());
+
     }
 }
