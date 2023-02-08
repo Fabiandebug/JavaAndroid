8dab7bbe6c1cf51552a5a1e2815132d14ece2f7f
Maina Evans <kinuthiafabiankinuthia@gmail.com>
2023-02-08 14:22:18 +0300

    Using radiobtn,progressbar and checkbox

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    private RadioGroup rgMaritalStatus;
+    private ProgressBar progressbar;
 
     public void onBtnclick(View view){
 //        Initialize the text viewer
@@ -22,6 +27,35 @@ public class MainActivity extends AppCompatActivity {
         TextView vtxtlastname = findViewById(R.id.vtxtlastname);
         TextView vtxtemail = findViewById(R.id.vtxtemail);
         TextView  vtxtusername = findViewById(R.id.vtxtusername);
+
+//        Working with progressbar
+        progressbar=findViewById(R.id.progressBar);
+//        Working with radio groups
+        rgMaritalStatus= findViewById(R.id.rgMaritalStatus);
+        rgMaritalStatus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rbMarried:
+                        Toast.makeText(MainActivity.this, "You are married", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rbSingle:
+                        Toast.makeText(MainActivity.this, "You are single", Toast.LENGTH_SHORT).show();
+                        progressbar.setVisibility(View.VISIBLE);
+                        break;
+                    case R.id.rbInRel:
+                        Toast.makeText(MainActivity.this, "You are in a relationship", Toast.LENGTH_SHORT).show();
+                        progressbar.setVisibility(View.GONE);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+
+
+
 //Initialize the text editor
         EditText txtusername =findViewById(R.id.edittxtname);
         EditText txtfirstname = findViewById(R.id.edittxtFirstname);
